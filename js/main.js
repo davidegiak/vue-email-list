aa42069217ad71f3463bd1062db12c53b0ffb97a
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-15 16:22:07 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -3,25 +3,29 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        mail:null,
+        mail:[],
         indirizzo:"https://flynn.boolean.careers/exercises/api/random/mail"
       }
     },
     methods: {
         generatore() {
+          for (let i = 0; i <= 10; i++) {
+            const element = this.mail[i];
             axios.get(this.indirizzo).then((result) => {
                 let codiceRisposta = `${result.status}/${result.statusText}`;
                 let datiRisposta = result.data;
                 console.log("Ricevuta risposta", codiceRisposta, datiRisposta);
                 this.mail = datiRisposta.response;
+            
+              })
+          }
 
 
-            })
         }
     },
     mounted() {
-        console.log("App montata");
-
+        console.log("App montata");  
         this.generatore();
+        console.log(this.mail);
     }
   }).mount('#app')
